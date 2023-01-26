163538d5cbc7a515d05d8b96d13e707cd56380b2
Allan <a11anr3y3s@gmail.com>
2023-01-26 00:46:44 -0600

    Add logo and web name

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ export function Header() {
             <nav>
                 <ul>
                     
-                    <li id="nav-item"><a><img src="https://imgtr.ee/images/2023/01/26/GZ2ts.png"></img>Login/Register</a></li>
+                    <li id="nav-item"><a><img src="https://imgtr.ee/images/2023/01/26/GZ2ts.png" alt="Profile Logo"></img>Ingresar/Registrarse</a></li>
                     <li id="nav-item"><a href="#"><h1>Judo Coronado</h1></a></li>
-                    <li id="nav-item"><a>About</a></li>
+                    <li id="nav-item"><a>Sobre Nosotros</a></li>
                 </ul>
             </nav>
         </div>
