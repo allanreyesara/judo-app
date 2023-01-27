9ac002c1ffe87433bd31d0a922e7ee8341451180
Allan <a11anr3y3s@gmail.com>
2023-01-26 23:07:04 -0600

    Add Media query CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,19 +11,25 @@ export function Header() {
         <Router>
                 <div className="header">
                     <nav className="navbar">
-                        <ul>
-                            <div className="navigation-menu">
-                                <li id="nav-item">
-                                    <img src="https://imgtr.ee/images/2023/01/26/GZ2ts.png" alt="Profile Logo" />
-                                    <ReactRouterLink to="/profile" component={ Link}> Ingresar/Registrarse </ReactRouterLink>
-                                </li>
-                                <li id="nav-item-title">
-                                    <h1><ReactRouterLink to="/" component= { Link }> Judo Coronado </ReactRouterLink></h1>
-                                </li>
-                                <li id="nav-item-options">
-                                    <ReactRouterLink to="/about" component= { Link}>Sobre Nosotros</ReactRouterLink>
-                                </li>
-                            </div>
+                        
+                        <a href="#navigation-menu" className="hamburger" >&#9776;</a>
+
+                        <ul id="navigation-menu" className="navigation-menu">
+                        
+                            <li className="nav-item">
+                                <img src="https://imgtr.ee/images/2023/01/26/GZ2ts.png" alt="Profile Logo" />
+                                <ReactRouterLink to="/profile" component={ Link }> Ingresar/Registrarse </ReactRouterLink>
+                            </li>
+                            <li className="nav-item-title">
+                                <h1><ReactRouterLink to="/" component= { Link }> Judo Coronado </ReactRouterLink></h1>
+                            </li>
+                            <li className="nav-item">
+                                <ReactRouterLink to="/about" component= { Link }>Sobre Nosotros</ReactRouterLink>
+                            </li>
+                            <a className="close" href="#">
+                                <img src="https://ljc-dev.github.io/testing0/ham-close.svg" alt="close" />
+                            </a>
+                        
                         </ul>
                     </nav>
                 </div>
