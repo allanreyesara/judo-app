eb0a823b01cd30faf1c58908c835e7693fbbf9e3
Allan <a11anr3y3s@gmail.com>
2023-02-23 01:29:06 -0600

    Add queries methods test

--- src/components/News.js
+++ src/components/News.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
+import { API, Predicates } from 'aws-amplify';
 import { DataStore } from 'aws-amplify';
 import { Noticias } from '../models';
 
 
 const NewsFeed = () => {
         const [newsData, setNewsData] = useState([])
-    
-    
+
         useEffect(() => {
             async function getArticles() {
-                try {
-                    const articles = await DataStore.query(Noticias);
-                    console.log(articles);
-                }catch(err){
+                try{
+                    const models = await DataStore.query(Noticias, Predicates.ALL);
+                    console.log("Noticias recuperadas correctamente", models);
+                    setNewsData(JSON.stringify(models, null, 5));
+                } catch(err){
                     console.log('Error cargando las noticias', err)
-                }
+                }  
             }
-        })
-    
+
+            getArticles();
+        }, [newsData]);
+
         return (
             <div className='newsFeed'>
-                {newsData.map((item, index) => <p key={item}>{item}</p>)}
+                
             </div>
         )};
 
-export default NewsFeed;
+ export default NewsFeed;
