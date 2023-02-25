8881cc1e99e1748614c033485cb5b2e43e8ede02
Allan <a11anr3y3s@gmail.com>
2023-02-25 08:49:15 -0600

    Delete test

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,23 +7,24 @@ import '@aws-amplify/ui-react/styles.css';
 import { DataStore } from '@aws-amplify/datastore';
 import { Noticias } from '../models';
 
-async function botonCrearNoticia(){
-  await DataStore.save(
-    new Noticias({
-		"titulo": "Lorem ipsum dolor sit amet",
-		"contenido": "Lorem ipsum dolor sit amet",
-		"imagen": "Lorem ipsum dolor sit amet"
-	})
-);
-}
-function Profile({ signOut }) {
+//async function botonCrearNoticia(){
+ // await DataStore.save(
+   // new Noticias({
+		//"titulo": "",
+		//"contenido": "Lorem ipsum dolor sit amet",
+		//"imagen": "Lorem ipsum dolor sit amet"
+	//})
+//);
+//}
+
+function Profile({ signOut }, props) {
   return(
     <div className='App'>
       <div className='body'>
         <body>
           <img src='https://www.linkpicture.com/q/judoCoroBuilding.jpg' className="judoCoro" alt="Judo Coronado Group"/>
           <h1> Esta página web se encuentra en construcción, ¡te esperamos pronto!</h1>
-          <button onClick={botonCrearNoticia()}>Crear Noticia</button>
+          
           <button onClick={signOut}>Sign Out</button>
         </body>
       </div>
