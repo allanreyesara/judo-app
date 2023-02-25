41d26f3055fa8b38c84dcfad7da6a015fbb02297
Allan <a11anr3y3s@gmail.com>
2023-02-25 08:04:59 -0600

    Add test

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,18 @@ import './building.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import { DataStore } from '@aws-amplify/datastore';
+import { Noticias } from '../models';
+
+async function botonCrearNoticia(){
+  await DataStore.save(
+    new Noticias({
+		"titulo": "Lorem ipsum dolor sit amet",
+		"contenido": "Lorem ipsum dolor sit amet",
+		"imagen": "Lorem ipsum dolor sit amet"
+	})
+);
+}
 function Profile({ signOut }) {
   return(
     <div className='App'>
@@ -11,6 +23,7 @@ function Profile({ signOut }) {
         <body>
           <img src='https://www.linkpicture.com/q/judoCoroBuilding.jpg' className="judoCoro" alt="Judo Coronado Group"/>
           <h1> Esta página web se encuentra en construcción, ¡te esperamos pronto!</h1>
+          <button onClick={botonCrearNoticia()}>Crear Noticia</button>
           <button onClick={signOut}>Sign Out</button>
         </body>
       </div>
